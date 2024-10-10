8548838e2ac165a890c1b584d66b580e4ce7a3e8
madhava <madhavavukkum@gmail.com>
2024-10-10 21:07:16 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Include .jsx files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
